7c80b5afd5b2b8d89117fb933b336913732ec1aa
choign3123 <choign3123@naver.com>
2023-02-27 13:43:02 +0900

    깃이그노어 적용 확인

--- src/main/java/capstone/bapool/TestController.java
+++ src/main/java/capstone/bapool/TestController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -11,5 +14,15 @@ public class TestController {
     @GetMapping("/log")
     public String test(){
         return "밥풀 테스트 요청 성공";
+    }
+
+    @GetMapping("/list")
+    public List<Integer> testList(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        return list;
     }
 }
