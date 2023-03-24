51f0dde94b507f7e9fe5992c46a86e7e636dfbe8
Kim jaeung <bes04172qwer@naver.com>
2023-03-24 20:56:11 +0900

    swaggerconfig 추가

--- src/main/java/capstone/bapool/config/swagger/SwaggerConfig.java
+++ src/main/java/capstone/bapool/config/swagger/SwaggerConfig.java
@@ -7,16 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -32,7 +30,9 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        Server bapoolServer = new Server("bapool", "https://myfirstdomain.store", "for testing", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.SWAGGER_2)
+                .servers(bapoolServer)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
