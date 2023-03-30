63b9ac0b3db0a0917529ec0a3122ef0dc66444bd
choign3123 <choign3123@naver.com>
2023-03-30 18:47:13 +0900

    clore: 카카오 로컬 api 테스트

--- src/test/java/capstone/bapool/KakaoLocalApiTests.java
+++ src/test/java/capstone/bapool/KakaoLocalApiTests.java
@@ -37,12 +37,17 @@ public class KakaoLocalApiTests {
 
             System.out.println("2");
 
+            conn.connect();
+
             //결과 코드가 200이라면 성공
             int responseCode = conn.getResponseCode();
+            System.out.println("conn.getResponseCode() = " + conn.getResponseCode());
 //            log.info("responseCode : {}" , responseCode);
 //            log.info(conn.getResponseMessage());
 
             // 200 아닐경우 예외처리 필요
+
+            System.out.println("conn.getResponseMessage() = " + conn.getResponseMessage());
 
             //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
@@ -57,8 +62,12 @@ public class KakaoLocalApiTests {
             System.out.println("result = " + result);
 
             //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-            JsonParser parser = new JsonParser();
+            JsonParser parser = new JsonParser(); //static 메소드를 쓰면 되서 저럼.
             JsonElement element = parser.parse(result);
+
+            element = JsonParser.parseString(result);
+
+            //KakaoLacalApiRes documents = element.getAsJsonObject().get("documents").getAsJsonObject();
 
             // 응답 바디에서 토큰값 읽어오기
 //            String accessToken = element.getAsJsonObject().get("access_token").getAsString();
