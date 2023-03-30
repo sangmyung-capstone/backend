9aac0e60dfc44113768281b775c375e720ceef06
kimjaeung <bes04172qwer@naver.com>
2023-03-30 20:46:53 +0900

    서비스 내용추가

--- src/main/java/capstone/bapool/restaurant/RestaurantController.java
+++ src/main/java/capstone/bapool/restaurant/RestaurantController.java
@@ -8,15 +8,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/restaurants")
 public class RestaurantController {
     @GetMapping("/{user-id}")
-    public GetRestaurantInfoRes 식당정보(@PathVariable(name = "user-id")Long userId, @RequestParam String rect){
+    public GetRestaurantInfoRes getRestaurantInfo(@PathVariable(name = "user-id")Long userId, @RequestParam String rect){
         System.out.println("userId = " + userId);
         System.out.println("rect = " + rect);
-        RestaurantInfo restaurants  = new RestaurantInfo(1l,"a","b","c","d",1,1d,1d);
-        GetRestaurantInfoRes abc = new GetRestaurantInfoRes();
-        abc.addrestaurant(restaurants);
-        RestaurantInfo restaurantss  = new RestaurantInfo(1l,"a","b","c","d",1,1d,1d);
-        abc.addrestaurant(restaurantss);
 
-        return abc;
+        return restaurantService.getRestaurantInfo();
     }
+
+    RestaurantService restaurantService;
+    public RestaurantController(RestaurantService restaurantService){
+        this.restaurantService = restaurantService;
+    }
+
 }
