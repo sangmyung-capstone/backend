676be9ef23607201411fffd2197ee07e871dcce6
김현재 <41482946+galug@users.noreply.github.com>
2023-05-15 16:57:20 +0900

    fix(user): 유저 관련 로직 수정

--- src/main/java/capstone/bapool/user/UserController.java
+++ src/main/java/capstone/bapool/user/UserController.java
@@ -5,22 +5,27 @@ import capstone.bapool.config.response.ResponseDto;
 import capstone.bapool.user.dto.ReissueReq;
 import capstone.bapool.user.dto.ReissueRes;
 import capstone.bapool.user.dto.SignUpReq;
+import capstone.bapool.user.dto.SignupRes;
 import capstone.bapool.user.dto.SocialAccessToken;
 import capstone.bapool.utils.JwtUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -56,7 +61,6 @@ public class UserController {
 
     @PostMapping(value = "/naver/signup")
     public ResponseEntity<ResponseDto> SignUpNaver(@RequestBody SignUpReq signUpReq) throws BaseException, IOException {
-        log.info("네이버 회원 가입 ");
         ReissueRes signupRes = userService.signUpNaver(signUpReq);
         return ResponseEntity.ok().body(ResponseDto.res(signupRes));
     }
@@ -71,5 +75,4 @@ public class UserController {
         ReissueRes signupRes = userService.reissueAccessToken(reissueReq);
         return ResponseEntity.ok().body(ResponseDto.res(signupRes));
     }
-
 }
