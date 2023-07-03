bc41f117fa509c6f9098794004d60ead66a0d884
choign3123 <choign3123@naver.com>
2023-07-03 21:49:35 +0900

    chore: @EnableJpaAuditing 어노테이션 추가

--- src/main/java/capstone/bapool/BapoolApplication.java
+++ src/main/java/capstone/bapool/BapoolApplication.java
@@ -2,8 +2,10 @@ package capstone.bapool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BapoolApplication {
 
 	public static void main(String[] args) {
