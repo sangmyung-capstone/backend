501a94987f4269475abaf2a8e72df58cad249662
Kim jaeung <bes04172qwer@naver.com>
2023-07-11 16:35:10 +0900

    View other users jaewoong (#30)
    * feat:마이페이지,회원탈퇴
    
    * chore:delete response 수정
    
    * chore:firebase userdelete추가 in userService.deletebyid
    
    * chore:blockuerrepository query 수정
    
    * chore:dto수정
    
    * chore:중간 저장
    
    * view otheruser, block otheruser 기본틀 완성
    
    * 차단시간 빼고 완료
    
    * feat:토큰빼고 완성

---
+++ src/main/java/capstone/bapool/user/dto/OtherUserReq.java
@@ -0,0 +1,15 @@
+package capstone.bapool.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OtherUserReq {
+
+    @JsonProperty("OtherUserId")
+    Long otherUserId;
+}
