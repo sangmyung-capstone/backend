c673fd73fe3d4479ecd554f04129feb67db74c32
choign3123 <choign3123@naver.com>
2023-07-17 16:58:32 +0900

    fix: user에 /test 추가

--- src/main/java/capstone/bapool/user/UserController.java
+++ src/main/java/capstone/bapool/user/UserController.java
@@ -23,7 +23,7 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/users")
+@RequestMapping(path = {"/users", "/test/users"})
 public class UserController {
 
     private final UserService userService;
