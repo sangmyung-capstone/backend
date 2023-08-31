7b989bfb7b85c58924adad017bf79b1f78fef66a
kimjaeung <bes04172qwer@naver.com>
2023-08-31 11:43:05 +0900

    fix:닉네임 변경 blank check

--- src/main/java/capstone/bapool/user/dto/UserInfoReq.java
+++ src/main/java/capstone/bapool/user/dto/UserInfoReq.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserInfoReq {
+    @NotBlank(message = "공백이 아닌 이름을 사용해야 합니다")
     @JsonProperty("nickname")
     String name;
 
